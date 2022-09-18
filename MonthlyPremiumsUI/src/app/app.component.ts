a7400ad321976c904d933ebfbc98ee7f30ff186c
brathore88 <bhupendra.rathore882@gmail.com>
2022-09-18 14:07:49 +0530

    Added Occupation Dropdown and Death Premium

--- MonthlyPremiumsUI/src/app/app.component.ts
+++ MonthlyPremiumsUI/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'MonthlyPremiums';
+  isSubmitted = false;
+  Occupations: any = ['Cleaner', 'Doctor', 'Author', 'Farmer', 'Mechanic', 'Florist'];
+  constructor(public fb: FormBuilder) {}
+  registrationForm = this.fb.group({
+    OccupationType: ['', [Validators.required]],
+  });
+  changeOccupation(e: any) {
+    this.OccupationType?.setValue(e.target.value, {
+      onlySelf: true,
+    });
+    console.log(e.target.value);
+
+  }
+  // Access formcontrols getter
+  get OccupationType() {
+    return this.registrationForm.get('OccupationType');
+  }
+  onSubmit(): void {
+    console.log(this.registrationForm);
+    this.isSubmitted = true;
+    if (!this.registrationForm.valid) {
+      false;
+    } else {
+      console.log(JSON.stringify(this.registrationForm.value));
+    }
+  }
 }
